25e88031532e81985dc51c14551b35aebe12a485
Bruce Hopewell <lbhopewell@outlook.com>
2018-09-04 20:06:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,5 +5,5 @@ const instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
 function theBeatlesPlay(musicians,instruments) {
   let matches = [];
   
-  for(i=0)
+  for(i=0; musicians)
 }
