818ff887e8aa5706465874de7a24083616a35657
Bruce Hopewell <lbhopewell@outlook.com>
2018-09-04 20:37:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,6 +11,19 @@ function theBeatlesPlay(musicians,instruments) {
   return newArray;
 }
 
+const facts = [
+  "He was the last Beatle to learn to drive",
+  "He was never a vegetarian",
+  "He was a choir boy and boy scout",
+  "He hated the sound of his own voice"
+];
 
-
-function johnLennonFacts(facts)
+function johnLennonFacts(facts){
+  var i = 0
+  let shoutArray = []
+  while(i<facts.length){
+    shoutArray.push(facts[i] + '!!!');
+    i++;
+  }
+  return shoutArray;
+}
